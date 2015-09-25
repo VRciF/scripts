f02f2013358faa4355a50a023b8c44337f6ab9da
VRciF <vrcif0@gmail.com>
2015-09-25 10:45:21 +0200

    added license, little documentation and function synchronization example

--- cpp/synchronized.hpp
+++ cpp/synchronized.hpp
@@ -1,3 +1,48 @@
+/**
+ * Usage guidance:
+ *    Facts:
+ *        o) For every synchronized(){} block a memory address (pointer value)
+ *           is associated with a pthread_mutex_t and a reference counter
+ *        o) IF synchronized(somevar){} is used on a simple variable,
+ *           then it's address &somevar will be used
+ *        o) IF ptrToSomevar=&somevar; synchronized(&somevar){} OR synchronized(ptrToSomevar) is used on a pointer,
+ *           then the pointers address &somevar will be used
+ *        o) IF ptrToSomevar=&somevar; synchronized(&ptrToSomevar){} is used THEN
+ *           the variable ptrToSomevar gets locked! NOT somevar AND NOT &somevar
+ *           this is because, synchronized(&ptrToSomevar){} receives a
+ *           pointer to a pointer to somevar
+ *
+ * So keep in mind for a given variable, e.g. int somevar=0; then
+ *   synchronized(somevar){} AND synchronized(&somevar){}
+ * hold a lock an the exact same memory address,
+ * but synchronized(&ptrToSomevar){} won't
+ *
+ * Design Pattern:
+ *   to create have a whole function synchronized like it's done in java with
+ *   java: public synchronized void fooBar(){}
+ *   you can use static member's like
+ *   c++: void fooBar(){ Synchronized functionLock(__func__); }
+ *
+ * License:
+java like synchronized(){} keyword for c++
+Copyright (c) 2015, (VRciF, vrcif0@gmail.com). All rights reserved.
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 3.0 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library.
+* 
+* 
+* Full license: http://www.gnu.org/licenses/lgpl-3.0.txt
+ */
 #ifndef __SYNCRONIZED_HPP__
 #define __SYNCRONIZED_HPP__
 
@@ -14,11 +59,11 @@ class Synchronized{
             int counter;
         } metaMutex;
 
-        static pthread_mutex_t& getMutex(){
+        pthread_mutex_t& getMutex(){
             static pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
             return lock;
         }
-        static std::map<const void*, metaMutex*>& getMutexMap(){
+        std::map<const void*, metaMutex*>& getMutexMap(){
             static std::map<const void*, metaMutex*> mmap;
             return mmap;
         }
@@ -81,6 +126,6 @@ class Synchronized{
         }
 };
 
-#define syncronized(ptr) if(Synchronized sync = Synchronized(ptr))
+#define syncronized(ptr) if(Synchronized sync_##__LINE__ = Synchronized(ptr))
 
 #endif
--- cpp/test.synchronized.cpp
+++ cpp/test.synchronized.cpp
@@ -9,20 +9,32 @@
 
 std::map<std::string, std::string> container;
 
+void synchronizedFunction(){
+    Synchronized synToken(__func__);
+
+    std::cout << __func__ << std::endl;
+    sleep(5);
+}
+
 void* thread1(void *ptr){
     std::cout << "thread 1 started" << std::endl;
     syncronized(container){
         std::cout << "container is locked in thread 1" << std::endl;
         container.insert(std::make_pair("a", "b"));
         std::cout << "sleeping" << std::endl;
-        sleep(5);
+        sleep(3);
         std::cout << "insert again" << std::endl;
         container.insert(std::make_pair("c", "d"));
         std::cout << "leaving syncronization on thread1" << std::endl;
     }
+    std::cout << "thread 1 using synchronized function" << std::endl;
+    synchronizedFunction();
+    std::cout << "thread 1 finished synchronized function" << std::endl;
 }
 void* thread2(void *ptr){
-    std::cout << "thread 2 started" << std::endl;
+    std::cout << "thread 2 started - using synchronized function" << std::endl;
+    synchronizedFunction();
+    std::cout << "thread 2: synchronized function finished" << std::endl;
     syncronized(container){
         std::cout << "container is locked in thread 2" << std::endl;
         std::map<std::string,std::string>::iterator it = container.begin();
@@ -62,7 +74,7 @@ int main(int argc, char **argv){
         std::cout << "someptr addr: " << (someptr) << "=" << s2.getSyncronizedAddress() << std::endl;
         std::cout << "somevar addr: " << (&somevar) << "=" << s3.getSyncronizedAddress() << std::endl;
         std::cout << "s1 equals s3: " << s1.getSyncronizedAddress() << "=" << s3.getSyncronizedAddress() << std::endl;
-        std::cout << "s2 different s4: " << s2.getSyncronizedAddress() << "=" << s4.getSyncronizedAddress() << std::endl;
+        std::cout << "s2 different s4: " << s2.getSyncronizedAddress() << "!=" << s4.getSyncronizedAddress() << std::endl;
 
         std::cout << "Object test: " << (&object) << "=" << s7.getSyncronizedAddress() << std::endl;
         std::cout << "Object ptrtest: " << (&object) << "=" << s8.getSyncronizedAddress() << std::endl;
